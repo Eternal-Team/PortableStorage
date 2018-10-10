fc87f9582af0f9098aad146a3f0f75f7d2e03f6f
Itorius <Itorius@users.noreply.github.com>
2018-10-10 21:42:27 +0200

    Ammo Bags start

---
+++ Items/AmmoBags/DartHolder.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ContainerLibrary;
+using PortableStorage.Items.Bags;
+using PortableStorage.UI;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader.IO;
+
+namespace PortableStorage.Items
+{
+	public class DartHolder : BaseBag
+	{
+		public override Type UIType => typeof(TheBlackHolePanel);
+
+		public DartHolder()
+		{
+			handler = new ItemHandler(27);
+			handler.OnContentsChanged += slot =>
+			{
+				if (Main.netMode == NetmodeID.MultiplayerClient)
+				{
+					Player player = Main.player[item.owner];
+
+					List<Item> joined = player.inventory.Concat(player.armor).Concat(player.dye).Concat(player.miscEquips).Concat(player.miscDyes).Concat(player.bank.item).Concat(player.bank2.item).Concat(new[] { player.trashItem }).Concat(player.bank3.item).ToList();
+					int index = joined.FindIndex(x => x == item);
+					if (index < 0) return;
+
+					NetMessage.SendData(MessageID.SyncEquipment, number: item.owner, number2: index);
+				}
+			};
+		}
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("The Black Hole");
+			Tooltip.SetDefault($"Stores {handler.Slots} stacks of items");
+		}
+
+		public override void SetDefaults()
+		{
+			base.SetDefaults();
+
+			item.width = 32;
+			item.height = 32;
+		}
+
+		public override TagCompound Save() => new TagCompound
+		{
+			["Items"] = handler.Save()
+		};
+
+		public override void Load(TagCompound tag)
+		{
+			handler.Load(tag.GetCompound("Items"));
+		}
+
+		public override void NetSend(BinaryWriter writer) => TagIO.Write(Save(), writer);
+
+		public override void NetRecieve(BinaryReader reader) => Load(TagIO.Read(reader));
+	}
+}
