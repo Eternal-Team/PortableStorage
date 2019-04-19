97a4bd1a8fbe456ff20910011d5558de5e7a783c
Unknown <itorius.yt@gmail.com>
2019-04-19 11:50:18 +0200

    Remove the rest of Quantum content

--- Global/Utility.cs
+++ Global/Utility.cs
@@ -212,21 +212,7 @@ namespace PortableStorage.Global
 			Add("Coin", AmmoID.Coin);
 		}
 
-		public static Colors ColorFromItem(this Item item, Colors existing)
-		{
-			Main.LocalPlayer.noThrow = 2;
-			switch (item.type)
-			{
-				case ItemID.Diamond: return Colors.White;
-				case ItemID.Ruby: return Colors.Red;
-				case ItemID.Emerald: return Colors.Green;
-				case ItemID.Topaz: return Colors.Yellow;
-				case ItemID.Amethyst: return Colors.Purple;
-				case ItemID.Sapphire: return Colors.Blue;
-				case ItemID.Amber: return Colors.Orange;
-				default: return existing;
-			}
-		}
+		
 
 		public static RecipeGroup yoyoStringGroup;
 		public static RecipeGroup tier1HMBarsGroup;
