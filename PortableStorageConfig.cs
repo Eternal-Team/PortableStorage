71d31a77629609138c13a861e8146be915a748cb
Itorius <itorius.yt@gmail.com>
2022-05-18 16:41:10 +0200

    Add config

---
+++ PortableStorageConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using Terraria.ModLoader.Config;
+
+namespace PortableStorage;
+
+public class PortableStorageConfig: ModConfig
+{
+		public override ConfigScope Mode => ConfigScope.ClientSide;
+
+		[Header("$Mods.PortableStorage.Config.AlchemistBag")]
+		
+		[Label("$Mods.PortableStorage.Config.AlchemistBagQuickBuff")] 
+		[DefaultValue(true)] 
+		public bool AlchemistBagQuickBuff;
+		
+		[Label("$Mods.PortableStorage.Config.AlchemistBagQuickHeal")] 
+		[DefaultValue(true)] 
+		public bool AlchemistBagQuickHeal;
+		
+		[Label("$Mods.PortableStorage.Config.AlchemistBagQuickMana")] 
+		[DefaultValue(true)] 
+		public bool AlchemistBagQuickMana;
+}
