4dc91362b55fe1ad51a0328704796c326acea6c5
Itorius <itorius.yt@gmail.com>
2022-05-20 17:46:27 +0200

    Miner's Backpack

---
+++ Items/SpecialBags/MinersBackpack.cs
@@ -0,0 +1,64 @@
+﻿// modded ores courtesy of Vein Miner (will be different in 1.4, use as a reference)
+/*
+ThoriumMod,SmoothCoal,ThoriumOre,LifeQuartz,MagmaOre,Aquaite,LodeStone,ValadiumChunk,IllumiteChunk,SynthGold,SynthPlatinum,Opal,PearlStone,Onyx,PlacedGem
+SpiritMod,FloranOreTile,MarbleOre,GraniteOre,CryoliteOreTile,SpiritOreTile,ThermiteOre
+CalamityMod,AerialiteOre,CryonicOre,PerennialOre,CharredOre,AstralOre,ChaoticOre,UelibloomOre,AuricOre,SeaPrism
+Tremor,IceOre,AngeliteOreTile,ArgiteOre,CollapsiumOreTile,CometiteOreTile,FrostoneOreTile,HardCometiteOreTile,NightmareOreTile
+CheezeMod,StarlightOre
+Antiaris,EnchantedStone
+SacredTools,LapisOre,OblivionOreBlock,Flarium,BismuthOre,VenomiteOre
+Bluemagic,PuriumOre
+CrystiliumMod,RadiantOre
+AAMod,AbyssiumOre,IncineriteOre,EventideAbyssiumOre,DaybreakIncineriteOre,LuminiteOre,Apocalyptite,DynaskullOre,YtriumOre,UraniumOre,TechneciumOre,RadiumOre,PrismOre,HallowedOre,DoomitePlate
+UniverseOfSwordsMod,DamascusOreTile
+*/
+
+using ContainerLibrary;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PortableStorage.Items.SpecialBags;
+
+public class MinersBackpack : BaseBag
+{
+	private class MinersBackpackItemStorage : ItemStorage
+	{
+		private MinersBackpack bag;
+
+		public MinersBackpackItemStorage(int slots, MinersBackpack bag) : base(slots)
+		{
+			this.bag = bag;
+		}
+
+		// public void OnContentsChanged(object user, int slot, Item item)
+		// {
+		// 	Recipe.FindRecipes();
+		// 	Utility.SyncBag(bag);
+		// }
+
+		public override bool IsItemValid(int slot, Item item)
+		{
+			return Utility.OreWhitelist.Contains(item.type) || Utility.ExplosiveWhitelist.Contains(item.type);
+		}
+	}
+
+	public override string Texture => PortableStorage.AssetPath + "Textures/Items/MinersBackpack";
+
+	public override void OnCreate(ItemCreationContext context)
+	{
+		base.OnCreate(context);
+		
+		Storage = new MinersBackpackItemStorage(18, this);
+	}
+
+	public override void SetDefaults()
+	{
+		base.SetDefaults();
+
+		Item.width = 32;
+		Item.height = 32;
+		Item.rare = ItemRarityID.Green;
+		Item.value = Item.buyPrice(gold: 1);
+	}
+}
--- UI/MinersBackpackPanel.cs
+++ UI/MinersBackpackPanel.cs
@@ -1,33 +1,33 @@
-﻿// using BaseLibrary.UI;
-// using PortableStorage.Items.SpecialBags;
-//
-// namespace PortableStorage.UI
-// {
-// 	public class MinersBackpackPanel : BaseBagPanel<MinerBackpack>
-// 	{
-// 		public MinersBackpackPanel(MinerBackpack bag) : base(bag)
-// 		{
-// 			Width.Pixels = 12 + (SlotSize + SlotMargin) * 9;
-// 			Height.Pixels = 40 + (SlotSize + SlotMargin) * Container.Storage.Count / 9;
-//
-// 			UIGrid<UIContainerSlot> gridItems = new UIGrid<UIContainerSlot>(9)
-// 			{
-// 				Width = { Percent = 100 },
-// 				Height = { Pixels = -28, Percent = 100 },
-// 				Y = { Pixels = 28 },
-// 				Settings = { ItemMargin = SlotMargin }
-// 			};
-//
-// 			Add(gridItems);
-// 			for (int i = 0; i < Container.Storage.Count; i++)
-// 			{
-// 				UIContainerSlot slot = new UIContainerSlot(Container.Storage, i)
-// 				{
-// 					Width = { Pixels = SlotSize },
-// 					Height = { Pixels = SlotSize }
-// 				};
-// 				gridItems.Add(slot);
-// 			}
-// 		}
-// 	}
-// }
+﻿using BaseLibrary.UI;
+using PortableStorage.Items.SpecialBags;
+
+namespace PortableStorage.UI
+{
+	public class MinersBackpackPanel : BaseBagPanel<MinersBackpack>
+	{
+		public MinersBackpackPanel(MinersBackpack bag) : base(bag)
+		{
+			Width.Pixels = 12 + (SlotSize + SlotMargin) * 9;
+			Height.Pixels = 40 + (SlotSize + SlotMargin) * Container.GetItemStorage().Count / 9;
+
+			UIGrid<UIContainerSlot> gridItems = new UIGrid<UIContainerSlot>(9)
+			{
+				Width = { Percent = 100 },
+				Height = { Pixels = -28, Percent = 100 },
+				Y = { Pixels = 28 },
+				Settings = { ItemMargin = SlotMargin }
+			};
+
+			Add(gridItems);
+			for (int i = 0; i < Container.GetItemStorage().Count; i++)
+			{
+				UIContainerSlot slot = new UIContainerSlot(Container.GetItemStorage(), i)
+				{
+					Width = { Pixels = SlotSize },
+					Height = { Pixels = SlotSize }
+				};
+				gridItems.Add(slot);
+			}
+		}
+	}
+}
